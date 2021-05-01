7ff1446c140a8cb2afd05e647347549ea9603d43
Wenyu <wenyu.wang@helsinki.fi>
2021-05-01 21:51:49 +0300

    Update the information

--- R/aboutUsUI.R
+++ R/aboutUsUI.R
@@ -16,7 +16,7 @@ aboutUsUI <- function(id) {
             href = 'https://www.helsinki.fi/en/researchgroups/network-pharmacology-for-precision-medicine',
             target = '_blank'
           ),
-          " in the Research Programs Unit, the Faculty of Medicine at the ",
+          " in the Research Program of System Oncology, Faculty of Medicine at ",
           tags$a(
             "University of Helsinki",
             href = 'https://www.helsinki.fi/en',
@@ -24,13 +24,9 @@ aboutUsUI <- function(id) {
           ),
           ", Helsinki, Finland.",
           tags$br(), tags$br(),
-          "SynergyFinder is an interactive to evaluate the significance of combined drugs,",
-          " the observed drug combination responses are usually with expected responses ",
-          "calculated by using synergy scoring models deviation of observed and expected ",
-          "responses one may classify the drug combination as synergistic or antagonistic. ",
-          "It provides efficient implementations for all the popular synergy scoring models, ",
+          "SynergyFinder is an interactive tool for analyzing drug combination dose response data. It enables efficient implementations for all the popular synergy scoring models, ",
           "including HSA, Loewe, Bliss and ZIP to quantify the degree of drug synergy.",
-          "The R package 'synergyfinder' is available for R programmer."
+          "The R package 'synergyfinder' is available for R programmers."
         ),
         tags$h1(class = "bottom-blue", "OTHER TOOLS"),
         fluidRow(
@@ -41,7 +37,7 @@ aboutUsUI <- function(id) {
                 class = 'textCard left-light-green',
                 tags$b("synergyfinder R package"),
                 tags$br(),
-                "An R package for calculate and visualize synergy cores for drug combinations."
+                "An R package for calculating and visualizing synergy scores for drug combination screening data."
               ),
               tags$br(), tags$br(), tags$br(), tags$br(),
               href="https://www.bioconductor.org/packages/release/bioc/html/synergyfinder.html",
@@ -75,15 +71,33 @@ aboutUsUI <- function(id) {
               tags$br(),
               "Assistant Professor"
             ),
+ 
             tags$p(
-              class ="textCard left-light-orange",
-              tags$b("Jehad Aldahdooh"),
+              class='textCard left-light-orange',
+              tags$b("Wenyu Wang"),
               tags$br(),
-              "Server Manager",
+              "R Package Development",
               tags$br(),
               "PhD student"
+            ),
+            tags$p(
+              class ="textCard left-light-orange",
+              tags$b("Alina Malyutina"),
+              tags$br(),
+              "Method Development",
+              tags$br(),
+              "PhD student"
+            ),
+            tags$p(
+              class ="textCard left-light-orange",
+              tags$b("Alberto Pessia"),
+              tags$br(),
+              "Method Development",
+              tags$br(),
+              "Former postdoc Researcher"
             )
-          ),
+
+          )
           column(
             width = 6,
             tags$p(
@@ -95,13 +109,31 @@ aboutUsUI <- function(id) {
               "PhD student"
             ),
             tags$p(
-              class='textCard left-light-orange',
+              class ="textCard left-light-orange",
+              tags$b("Jehad Aldahdooh"),
+              tags$br(),
+              "Server Manager",
+              tags$br(),
+              "PhD student"
+            ),
+            tags$p(
+              class ="textCard left-light-orange",
+              tags$b("Tolou Shadbahr"),
+              tags$br(),
+              "Method Development",
+              tags$br(),
+              "Research assistant"
+            ),
+            tags$p(
+              class ="textCard left-light-orange",
               tags$b("Liye He"),
               tags$br(),
-              "R Package Development",
+              "Package developer",
               tags$br(),
-              "PostDoc"
+              "Former PhD student"
             )
+            
+           )
           )
         )
       )
