b630f6ead91955d71a86c1b4c96584195c5c1bff
Shuyu <shuyu.zheng@helsinki.fi>
2022-03-30 17:28:12 +0300

    Add one question into FAQ

--- R/faqUI.R
+++ R/faqUI.R
@@ -6,6 +6,33 @@ faqUI <- function(id) {
       width = 10,
       offset = 1,
       box(
+        id = "faq2",
+        title = "How do I report a technical issue on SynergyFinder?",
+        solidHeader = TRUE,
+        width =12,
+        height = NULL,
+        collapsible = TRUE,
+        collapsed = TRUE,
+        fluidRow(
+          tags$div(
+            class = "faq",
+            tags$p(
+              "You can report the technical issues to our GitHub repositories: ",
+              tags$a(
+                href = "https://github.com/TangSoftwareLab/SynergyFinderWeb/issues",
+                "TangSoftwareLab/SynergyFinderWeb",
+                target="_blank"),
+              "web application, or ",
+              tags$a(
+                href = "https://github.com/TangSoftwareLab/SynergyFinderR/issues",
+                "TangSoftwareLab/SynergyFinderR",
+                target="_blank"),
+              "for R package."
+            )
+          )
+        )
+      ),
+      box(
         id = "faq1",
         title = "Mathematical modelling in SynergyFinderPlus and SynergyFinder2",
         solidHeader = TRUE,
